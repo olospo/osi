a7b47df19c08e1c85c1e1946d1c25ec8d92712a9
Tom Brooks <tom@olospo.co.uk>
2020-09-28 15:33:50 +0100

    Home updates

--- templates/home.php
+++ templates/home.php
@@ -3,11 +3,28 @@ get_header();
 
 while ( have_posts() ) : the_post(); ?>
 
-
-
-
 <section class="cats">
   <div class="container">
+    
+    <?php if( have_rows('study') ): ?>
+      <?php while( have_rows('logo') ): the_row(); 
+        // Fields
+        $cat = get_sub_field('cat_image');
+        $studyLogo = get_sub_field('study_logo');
+        $studyText = get_sub_field('study_text');
+        $studyButtonText = get_sub_field('study_button_text');
+        $studyButtonLink = get_sub_field('study_button_link');
+      ?>
+      <div class="cat_study one-third column">
+        <img src="<?php echo esc_url($cat['url']); ?>" alt="<?php echo esc_attr($cat['alt']); ?>" class="cat" />
+        <img src="<?php echo esc_url($studyLogo['url']); ?>" alt="<?php echo esc_attr($studyLogo['alt']); ?>" class="logo" />
+        <?php echo $studyText; ?>
+        <a href="<?php echo $studyButtonLink; ?>" class="button primary"><?php echo $studyButtonText; ?></a>
+      </div>
+      <?php endwhile; ?>
+    <?php endif; ?>
+    
+    
     <div class="cat_study one-third column">
       <img src="<?php bloginfo('template_directory'); ?>/img/icats_cat.png" alt="iCats Cat" class="cat" />
       <img src="<?php bloginfo('template_directory'); ?>/img/icats.png" alt="iCats" class="logo" />
@@ -29,26 +46,43 @@ while ( have_posts() ) : the_post(); ?>
   </div>
 </section>
 
+<?php 
+  // About/What Section 
+  $title = get_field('title');
+  $content = get_field('content');
+  $buttonText = get_field('button_text');
+  $buttonLink = get_field('button_link');
+?>
 <section class="what">
   <div class="container">
     <div class="ten columns offset-by-one">
-      <h2>Online Support and Intervention for Child Anxiety</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
-      <a href="#" class="button secondary">Find out more</a>
+      <h2><?php echo $title; ?></h2>
+      <?php echo $content; ?>
+      <a href="<?php echo $buttonLink; ?>" class="button secondary"><?php echo $buttonText; ?></a>
     </div>
   </div> 
 </section>
+
+<?php 
+  // Logo/Funded Section 
+  $logo = get_sub_field('logo_image');
+?>
 
 <section class="funded">
   <div class="container">
     <div class="twelve columns">
       <div class="funding_logos">
-        <ul>
-          <li><img src="<?php bloginfo('template_directory'); ?>/img/nhs_logo.jpg" alt="NHS Oxford Health" class="logo" /></li>
-          <li><img src="<?php bloginfo('template_directory'); ?>/img/ep_logo.jpg" alt="Experimental Phychology" class="logo" /></li>
-          <li><img src="<?php bloginfo('template_directory'); ?>/img/nihr_logo.png" alt="National Insitutre for Health Research" class="logo" /></li>
-          <li><img src="<?php bloginfo('template_directory'); ?>/img/oxforduni_logo.png" alt="Oxford University" class="logo" /></li>
-        </ul>
+        <?php if( have_rows('logo') ): ?>
+          <ul>
+          <?php while( have_rows('logo') ): the_row(); 
+              $logo = get_sub_field('logo_image');
+              ?>
+              <li>
+                  <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>" class="logo" />
+              </li>
+          <?php endwhile; ?>
+          </ul>
+        <?php endif; ?>
       </div>
     </div>
   </div>
